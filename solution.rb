7986788de2b49a7955cbd3959a69ad2814653b90
Icorrea7 <isacorrea07@gmail.com>
2018-05-30 11:07:26 -0500

    abuela sorda

--- solution.rb
+++ solution.rb
@@ -2,3 +2,13 @@ require 'sinatra'
 get '/' do #cuando ingrese a la url hace lo siguiente
   erb :hola
 end
+post '/respuesta' do
+  respuesta = params[:frase]
+  if respuesta.upcase == respuesta
+    @respuesta = "Ahhh si, manzanas!"
+    redirect "/?respuesta=#{@respuesta}"
+  else
+    @respuesta = "Habla más duro mijito"
+    redirect "/?respuesta=#{@respuesta}"
+  end
+end
