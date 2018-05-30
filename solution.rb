3e9b95c665f48394bc66aac9d132db7631685a47
Icorrea7 <isacorrea07@gmail.com>
2018-05-30 14:03:07 -0500

    http navegador

--- solution.rb
+++ solution.rb
@@ -1,14 +1,16 @@
 require 'sinatra'
 get '/' do #cuando ingrese a la url hace lo siguiente
-  erb :hola
+  @user =request.user_agent
+
+  # erb :hola
 end
-post '/respuesta' do
-  respuesta = params[:frase]
-  if respuesta.upcase == respuesta
-    @respuesta = "Ahhh si, manzanas!"
-    redirect "/?respuesta=#{@respuesta}"
-  else
-    @respuesta = "Habla mas duro mijito"
-    redirect "/?respuesta=#{@respuesta}"
-  end
-end
+# post '/respuesta' do
+#   respuesta = params[:frase]
+#   if respuesta.upcase == respuesta
+#     @respuesta = "Ahhh si, manzanas!"
+#     redirect "/?respuesta=#{@respuesta}"
+#   else
+#     @respuesta = "Habla mas duro mijito"
+#     redirect "/?respuesta=#{@respuesta}"
+#   end
+# end
